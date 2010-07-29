56880d607431e93bb422c2aea9444a141d623722
Keith Stribley <devel@thanlwinsoft.org>
2010-07-29 20:35:47 +0630

    add locale2lcid files

---
+++ mkdeb.sh
@@ -0,0 +1,21 @@
+DEB_DIR=../grooodeb`uname -m`/
+ARCHIVE_DIR=../grooodeb
+PKG_VER=`tail -1 .hgtags | sed "s/.* //"`
+SRC_DIR=`pwd`
+mkdir -p ${ARCHIVE_DIR}
+mkdir -p ${DEB_DIR}
+rm -rf ${DEB_DIR}*
+if test -f ${ARCHIVE_DIR}/openoffice.org-graphite_${PKG_VER}.orig.tar.gz;
+then
+    cp ${ARCHIVE_DIR}/openoffice.org-graphite_${PKG_VER}.orig.tar.gz ${DEB_DIR}
+else
+    hg archive -X debian -X .hgtags -p openoffice.org-graphite_${PKG_VER} -ttgz ${ARCHIVE_DIR}/openoffice.org-graphite_${PKG_VER}.orig.tar.gz
+    cp ${ARCHIVE_DIR}/openoffice.org-graphite_${PKG_VER}.orig.tar.gz ${DEB_DIR}
+fi
+cd ${DEB_DIR}
+tar -zxf openoffice.org-graphite_${PKG_VER}.orig.tar.gz
+cd openoffice.org-graphite_${PKG_VER}
+cp -R ${SRC_DIR}/debian debian
+debuild
+cd ..
+
---
+++ src/test_locale2lcid.cxx
@@ -0,0 +1,93 @@
+/*************************************************************************
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Copyright 2010 ThanLwinSoft.org & SIL International
+ *
+ * This file is part of the Graphite extension for OpenOffice.org (GraphiteOOo).
+ *
+ * The GraphiteOOo Extension is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * The GraphiteOOo Extension is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
+ *
+ ************************************************************************/
+#include <cstdio>
+#include "locale2lcid.hxx"
+
+namespace osg = org::sil::graphite;
+
+bool test(::rtl::OUString lang, ::rtl::OUString country, ::rtl::OUString region, unsigned short expectedLcid, bool exact)
+{
+    static const osg::Locale2Lang locale2lcid;
+    unsigned short lcid = locale2lcid.getMsId(lang, country, region);
+    // check reverse
+    const osg::IsoLangEntry * entry = locale2lcid.findEntryById(lcid);
+    if (lcid == expectedLcid && entry &&
+        ((exact == false) ||
+         (lang.compareToAscii(entry->maLangStr) == 0 &&
+          country.compareToAscii(entry->maCountry) == 0)))
+    {
+        return true;
+    }
+    ::rtl::OString aLang;
+    ::rtl::OString aCountry;
+    lang.convertToString(&aLang, RTL_TEXTENCODING_UTF8, OUSTRING_TO_OSTRING_CVTFLAGS);
+    country.convertToString(&aCountry, RTL_TEXTENCODING_UTF8, OUSTRING_TO_OSTRING_CVTFLAGS);
+    printf("%s-%s expected %x got %x reverse %x\n", aLang.getStr(), aCountry.getStr(), expectedLcid, lcid,
+           entry? entry->mnLang : 0);
+    return false;
+}
+
+int main(int argc, char ** argv)
+{
+    
+    ::rtl::OUString lang = ::rtl::OUString::createFromAscii("en");
+    ::rtl::OUString country = ::rtl::OUString::createFromAscii("GB");
+    ::rtl::OUString region;
+    if (!test(lang, country, region, 0x809, true))
+    {
+        return 1;
+    }
+    country = ::rtl::OUString::createFromAscii("MM");
+    if (!test(lang, country, region, 0x409, false))
+        return 2;
+
+    lang = ::rtl::OUString::createFromAscii("my");
+    if (!test(lang, country, region, 0x455, true))
+        return 3;
+
+    lang = ::rtl::OUString::createFromAscii("ar");
+    if (!test(lang, country, region, 0x401, false))
+        return 4;
+
+    country = ::rtl::OUString::createFromAscii("QA");
+    if (!test(lang, country, region, 0x4001, true))
+        return 5;
+
+    lang = ::rtl::OUString::createFromAscii("fil");
+    if (!test(lang, country, region, 0x464, false))
+        return 6;
+
+    country = ::rtl::OUString::createFromAscii("PH");
+    if (!test(lang, country, region, 0x464, true))
+        return 7;
+
+    // check last entry
+    lang = ::rtl::OUString::createFromAscii("es");
+    country = ::rtl::OUString::createFromAscii("US");
+    if (!test(lang, country, region, 0x540A, true))
+        return 8;
+    
+    return 0;
+}
